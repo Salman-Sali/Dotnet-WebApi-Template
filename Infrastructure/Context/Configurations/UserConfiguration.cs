41239f6dc8e45fc0bf2c96d892b19170029d9e74
Salman-Sali <salman.s@flotillatechs.com>
2023-05-02 19:00:36 +0530

    1

---
+++ Infrastructure/Context/Configurations/UserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.Entities;
+
+namespace Infrastructure.Context.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasIndex(a => a.Name)
+                .IsUnique();
+
+            builder
+                .HasData(
+                    User.Create("asdfasdf", "asdfasdfas")
+                );
+        }
+    }
+}
